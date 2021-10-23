a64788a8736eeb4449ad3080139c5bb231c1bc8a
rajithst <rajiththennakoonshady@gmail.com>
2021-10-23 23:11:22 +0900

    mutating method inside a struct

--- structures.playground/Contents.swift
+++ structures.playground/Contents.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+/*
+    Structs are use for custom data types.
+ */
 struct Town {
     
     let name: String
@@ -14,9 +17,19 @@ struct Town {
     func makeSong(){
         print("making a song")
     }
+    
+    /*
+        by default all properties are immutable inside the struct
+        if we need to mutate property through a method,we need to mark method as "mutating"
+    */
+    mutating func updateCitizens(){
+        citizens.append("kendrik")
+    }
 }
 
 
 var myTown = Town(townName:"rajith",people:["rajith","cat"],resources:["rajith":1,"cats":1])
 print(myTown.name)
 myTown.makeSong()
+myTown.updateCitizens()
+print(myTown.citizens)
